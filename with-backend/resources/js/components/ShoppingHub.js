9b0ec66f89374157c646d92ee817060c3edab990
kjuraszek <donkrissocorleone@gmail.com>
2021-04-14 20:54:10 +0200

    Update ShoppingHub.js
    - fetching shopping lists from the database
    - add/edit/delete shopping lists - all changes are permanently saved in the database
    - show loader while fetching the data
    - show error on failed fetching
    - some minor changes

--- with-backend/resources/js/components/ShoppingHub.js
+++ with-backend/resources/js/components/ShoppingHub.js
@@ -9,41 +9,15 @@ import AddShoppingListDialog from './AddShoppingListDialog';
 import ViewShoppingListDialog from './ViewShoppingListDialog';
 import EditShoppingListDialog from './EditShoppingListDialog';
 import ViewHelpDialog from './ViewHelpDialog';
-import { Container, Grid } from '@material-ui/core';
-
-// dummy-data
-const lists = [
-    {name: "Breakfast",
-    id: 0,
-    items: [{name:"eggs * 6", completed:true},{name:"Bread", completed:false},{name:"Butter", completed:true},{name:"Ham", completed:true}],
-    priority: 2,
-    completed: false
-    },
-    {name: "Amazon",
-    id: 1,
-    items: [{name:"SSD drive", completed:true},{name:"CDs", completed:false}],
-    priority: 3,
-    completed: false
-    },
-    {name: "Drugstore",
-    id: 2,
-    items: [{name:"vitamins: A D E K", completed:true},{name:"Headache pills", completed:true},{name:"Bandage", completed:true}],
-    priority: 4,
-    completed: true
-    },
-    {name: "Vegetables",
-    id: 3,
-    items: [{name:"Peppers * 2", completed:true},{name:"Onions * 3", completed:false},{name:"Cucumber", completed:true}],
-    priority: 1,
-    completed: false
-    }
-];
+import { Container, Box, Grid, CircularProgress } from '@material-ui/core';
+import HighlightOffSharpIcon from '@material-ui/icons/HighlightOffSharp';
+import axios from 'axios';
 
 class ShoppingHub extends React.Component{
     constructor(){
         super();
         this.state = {
-            lists: [...lists],
+            lists: [],
             addShoppingListDialog: false,
             viewShoppingListDialog: false,
             editShoppingListDialog: false,
@@ -51,8 +25,11 @@ class ShoppingHub extends React.Component{
             selectedList: false,
             hideCompletedLists: false,
             sortBy: "id",
-            reverseSorting: false
+            reverseSorting: false,
+            loading_data: true,
+            errors: false
         }
+
         this.addShoppingList = this.addShoppingList.bind(this);
         this.deleteShoppingList = this.deleteShoppingList.bind(this);
         this.toggleShoppingList = this.toggleShoppingList.bind(this);
@@ -65,17 +42,58 @@ class ShoppingHub extends React.Component{
         this.toggleViewHelpDialog = this.toggleViewHelpDialog.bind(this);
         this.toggleCompletedLists = this.toggleCompletedLists.bind(this);
         this.toggleSorting = this.toggleSorting.bind(this);
-        this.toggleReversedSorting = this.toggleReversedSorting.bind(this);
+        this.toggleReversedSorting = this.toggleReversedSorting.bind(this);     
     }
+
+    componentDidMount () {
+        axios.get('/api/shopping_lists')
+        .then(response => {
+            let lists = response.data;
+            axios.get('/api/shopping_items')
+            .then(response => {
+                let items = response.data;
+                lists.forEach((shopping_list, index)=>{
+                    shopping_list.completed = shopping_list.completed === 0 ?  false : true;
+                    shopping_list.items = items
+                    .filter( (shopping_item) => shopping_item.shopping_list_id === shopping_list.id )
+                    .map( (shopping_item) => {
+                        shopping_item.completed = shopping_item.completed === 0 ?  false : true;
+                        return shopping_item;
+                    });
+                });
+                this.setState({
+                    lists: lists,
+                    loading_data: false
+                });
+            })
+            
+        })
+        .catch(error => {
+            console.log(error);
+            this.setState({
+              errors: true,
+              loading_data: false
+            })
+        });
+    }
+
     addShoppingList(listAdded){
-        let lists = this.state.lists;
-        listAdded.id = lists.length > 0 ? 
-        lists.sort(
-            (a,b) => b.id - a.id
-        )[0].id + 1 : 0;
-        this.setState(state => ({
-            lists: state.lists.concat([listAdded])
-        }))       
+        this.setState({
+            loading_data: true
+        });
+        listAdded.completed = false;
+        axios.post('/api/shopping_lists', listAdded).then(response => {
+            listAdded.id = response.data.id;
+            axios.post(`/api/shopping_items/save`, listAdded).then(response => {
+                this.setState(state => ({
+                    lists: state.lists.concat([listAdded]),
+                    loading_data: false
+                }));
+            });        
+        })
+        .catch(error => {
+            console.log(error); 
+        });        
     }
     editShoppingList(listEdited){
         this.setState({
@@ -84,21 +102,32 @@ class ShoppingHub extends React.Component{
         this.toggleEditShoppingListDialog();
     }
     saveShoppingList(listSaved){
-        this.setState((state) => ({
-            lists: state.lists.map((list) => {
-                if(list.id !== listSaved.id ){
-                    return list;
-                } else {
-                    // automatically complete list if all items are completed
-                    if( listSaved.items.length > 0 && listSaved.items.filter( (item) => !item.completed).length === 0){
-                        listSaved.completed = true;
-                    } else {
-                        listSaved.completed = false;
-                    }
-                    return listSaved;
-                }
-            })
-        }));      
+        // automatically complete list if all items are completed
+        if( listSaved.items.length > 0 && listSaved.items.filter( (item) => !item.completed).length === 0){
+            listSaved.completed = true;
+        } else {
+            listSaved.completed = false;
+        }
+        this.setState({
+            loading_data: true
+        });
+        axios.post(`/api/shopping_lists/save`, listSaved).then(response => {
+            axios.post(`/api/shopping_items/save`, listSaved).then(response => {
+                this.setState((state) => ({
+                    lists: state.lists.map((list) => {
+                        if(list.id !== listSaved.id ){
+                            return list;
+                        } else {
+                            return listSaved;
+                        }
+                    }),
+                    loading_data: false
+                }));
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
     viewShoppingList(listViewed){
         this.setState({
@@ -107,26 +136,50 @@ class ShoppingHub extends React.Component{
         this.toggleViewShoppingListDialog();
     }
     deleteShoppingList(listDeleted){
-        this.setState((state) => ({
-            lists: state.lists.filter(list => list.id !== listDeleted.id )
-        }))
+        this.setState({
+            loading_data: true
+        });
+        axios.post(`/api/shopping_lists/${listDeleted.id}/delete`).then(response => {
+            axios.post(`/api/shopping_items/${listDeleted.id}/delete`).then(response => {
+                this.setState((state) => ({
+                    lists: state.lists.filter(list => list.id !== listDeleted.id ),
+                    loading_data: false
+                }))
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
     toggleShoppingList(listToggled){
-        this.setState((state) => ({
-            lists: state.lists.map((list) => { 
-                if(list.id !== listToggled.id ){
-                    return list;
-                } else {
-                    let newList = {...list};
-                    newList.completed = !list.completed;
-                    newList.items = newList.items.map((item) => {
-                        item.completed = newList.completed;
-                        return item;
-                    });
-                    return newList;
-                }
-            })
-        }));
+        this.setState({
+            loading_data: true
+        });
+        let newList = {...listToggled};
+        newList.completed = !listToggled.completed;
+        newList.items = newList.items.map((item) => {
+            item.completed = newList.completed;
+            return item;
+        });
+
+        axios.post(`/api/shopping_lists/save`, newList).then(response => {
+            axios.post(`/api/shopping_items/save`, newList).then(response => {
+                this.setState((state) => ({
+                    lists: state.lists.map((list) => { 
+                        if(list.id !== newList.id ){
+                            return list;
+                        } else {
+                            return newList;
+                        }
+                    }),
+                    loading_data: false
+                }));
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+ 
     }
     toggleAddShoppingListDialog(){
         this.setState((state) => ({
@@ -175,15 +228,60 @@ class ShoppingHub extends React.Component{
                     <Grid item xs={12} sm={8} lg={9}>
                         <Container>
                         <h1>ShoppingHub</h1>
+
+                        {!this.state.loading_data && 
+                        !this.state.errors && 
                         <ShoppingHubControls 
                         toggleCompletedLists={this.toggleCompletedLists}
                         toggleSorting={this.toggleSorting}
                         toggleReversedSorting={this.toggleReversedSorting}
                         reverseSorting={this.state.reverseSorting}
-                        sortBy={this.state.sortBy}/>
-                        {this.state.lists.length === 0 && <p>You have no shopping lists. Click a button below to add one!</p> }
+                        hideCompletedLists={this.state.hideCompletedLists}
+                        sortBy={this.state.sortBy}/>}
+
+                        {!this.state.loading_data  &&
+                        this.state.errors &&  
+                        <Box
+                            margin="15px"
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            flexDirection="column"
+                        >
+                            <HighlightOffSharpIcon fontSize="large" color="error"/>
+                            <p>ShoppingHub was unable do connect to the database</p>
+                        </Box>}
+
+                        {this.state.loading_data  &&
+                        !this.state.errors &&  
+                        <Box
+                            margin="15px"
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            flexDirection="column"
+                        >
+                            <CircularProgress />
+                            <p>Loading Your Shopping Lists...</p>
+                        </Box>}
+
+                        {!this.state.loading_data && 
+                        !this.state.errors && 
+                        this.state.lists.length === 0 && 
+                        <Box
+                            margin="15px"
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            flexDirection="column"
+                        >
+                            <p>You have no shopping lists. Click a button below to add one!</p>
+                        </Box> }
+
                         <Grid container spacing={3}  alignItems="stretch" >
-                            {this.state.lists.filter(
+                            {!this.state.loading_data && 
+                            !this.state.errors && 
+                            this.state.lists.filter(
                                 // hide completed lists if set
                                 (item => !this.state.hideCompletedLists || !item.completed)).sort(
                                     // sort lists by sortBy
@@ -218,9 +316,12 @@ class ShoppingHub extends React.Component{
                                         onRemoveList={this.deleteShoppingList} />
                                 </Grid>)}
 
-                                <Grid item xs={12} lg={4} m={2} key={lists.length}>
-                                <ShoppingHubButton  m={2}
-                                onButtonClick={this.toggleAddShoppingListDialog} />
+                                <Grid item xs={12} lg={4} m={2} key={this.state.lists.length}>
+                                {!this.state.loading_data &&
+                                !this.state.errors && 
+                                <ShoppingHubButton  
+                                    m={2}
+                                    onButtonClick={this.toggleAddShoppingListDialog} />}
                                 </Grid>
                         </Grid>
                         <AddShoppingListDialog 
@@ -246,10 +347,12 @@ class ShoppingHub extends React.Component{
                     numberOfLists={this.state.lists.length}
                     numberOfCompletedLists={this.state.lists.filter( (list) => list.completed ).length}
                     numberOfItems={this.state.lists.reduce( (previousValue, currentList) => 
-                        previousValue + currentList.items.length, 0
+                        previousValue + (currentList.hasOwnProperty("items") ? currentList.items.length : 0), 0
                     )} 
-                    longestList={this.state.lists.sort(
-                        (a,b) => b.items.length - a.items.length
+                    longestList= {this.state.lists.sort(
+                        (a,b) => (b.hasOwnProperty("items") && a.hasOwnProperty("items") ? 
+                        b.items.length - a.items.length :
+                        b.id - a.id)
                     )[0]}/>
                     </Grid>
                 </Grid>
