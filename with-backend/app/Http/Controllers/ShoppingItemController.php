835caf3f0f2fb9cf1581d2a24ca9bd974ad182f8
kjuraszek <donkrissocorleone@gmail.com>
2021-04-14 20:56:48 +0200

    Update ShoppingItemController.php

--- with-backend/app/Http/Controllers/ShoppingItemController.php
+++ with-backend/app/Http/Controllers/ShoppingItemController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\ShoppingItem;
 use Illuminate\Http\Request;
 
-class ShoppingItemController extends Controller
-{
-    //
+class ShoppingItemController extends Controller{
+    public function index(){
+        $shopping_items = ShoppingItem::orderBy('id', 'ASC')->select('name', 'completed', 'shopping_list_id')->get();
+
+        return $shopping_items->toJson();
+    }
+
+    public function deleteShoppingItems($id){
+        $shopping_items = ShoppingItem::where('shopping_list_id', $id)->delete();
+    }
+
+    public function saveShoppingItems(Request $request){
+        $validatedData = $request->validate([
+          'id' => 'required',
+          'items' => 'present|array',
+        ]);
+
+        $old_shopping_items = ShoppingItem::where('shopping_list_id', $validatedData['id'])->delete();
+        
+        foreach($validatedData['items'] as $new_shopping_item){
+            $item = ShoppingItem::create([
+                'name' => $new_shopping_item['name'],
+                'shopping_list_id' => $validatedData['id'],
+                'completed' => $new_shopping_item['completed'],
+              ]);
+        }
+    }
 }
